d6312b81fbfe531b2246ed9b0d5a0610557f0b07
kamichidu <c.kamunagi@gmail.com>
2017-08-01 14:45:35 +0900

    Use junction for windows

--- hariti_windows.go
+++ hariti_windows.go
@@ -1,9 +1,17 @@
 package hariti
 
 import (
-	"os"
+	"bytes"
+	"errors"
+	"os/exec"
 )
 
 func mklink(oldname string, newname string) error {
-	return os.Link(oldname, newname)
+	stderr := new(bytes.Buffer)
+	cmd := exec.Command("cmd.exe", "/C", "mklink", "/J", newname, oldname)
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		return errors.New(stderr.String() + err.Error())
+	}
+	return nil
 }
